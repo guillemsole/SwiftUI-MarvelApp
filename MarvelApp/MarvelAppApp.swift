a778443c8a2a61fd995dba23f63aef7b904910b0
guillemsole <guillemsolecubilo@gmail.com>
2021-03-05 15:20:23 +0100

    Initial Commit

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Guillem Solé Cubilo on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
