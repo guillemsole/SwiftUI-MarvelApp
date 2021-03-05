13df7d1243cc5a893d9853126795e03d4ba86ddf
guillemsole <guillemsolecubilo@gmail.com>
2021-03-05 16:18:16 +0100

    ADD first URL Service approach

---
+++ MarvelApp/Server/MarvelAPI.swift
@@ -0,0 +1,8 @@
+//
+//  MarvelAPI.swift
+//  MarvelApp
+//
+//  Created by Guillem Solé Cubilo on 5/3/21.
+//
+
+import Foundation
---
+++ MarvelAppTests/MarvelAPITests.swift
@@ -0,0 +1,16 @@
+//
+//  MarvelAPI.swift
+//  MarvelAppTests
+//
+//  Created by Guillem Solé Cubilo on 5/3/21.
+//
+
+import XCTest
+@testable import MarvelApp
+
+class MarvelAPI: XCTestCase {
+
+    func test_auth_params() {
+        
+    }
+}
